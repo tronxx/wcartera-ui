3d6d4229866a7b1b31e4702fbc6cebff163c41ad
Kevin <bastokevindaniel@gmail.com>
2022-09-22 17:10:08 -0500

    feat: template routing added

---
+++ src/app/pages/client/table-list/table-list.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TableListComponent } from './table-list.component';
+
+
+
+@NgModule({
+  declarations: [
+    TableListComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class TableListModule { }
---
+++ src/app/pages/client/typography/typography.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TypographyComponent } from './typography.component';
+
+
+
+@NgModule({
+  declarations: [
+    TypographyComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class TypographyModule { }
