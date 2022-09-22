9fbb69cda78675f78596a2e6639b5a774d782a36
Kevin <bastokevindaniel@gmail.com>
2022-09-22 17:49:10 -0500

    feat: add sidebar component

--- src/app/components/shared-components/sidebar/sidebar.component.ts
+++ src/app/components/shared-components/sidebar/sidebar.component.ts
@@ -9,15 +9,28 @@ import { environment } from 'src/environments/environment';
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss'],
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent implements OnInit, AfterViewInit {
   public routes: Array<MenuItem> = environment.menu;
   public currentRoute : string;
   @Output() public toggle : EventEmitter<boolean> = new EventEmitter<boolean>();
+  public hideSidebar : boolean;
 
   constructor(
     private router: Router,
     public breakpoint : BreakpointObserver
   ) {}
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.breakpoint.observe(['(max-width: 800px)']).subscribe((res) => {
+        if (res.matches) {
+          this.hideSidebar = false;
+        } else {
+          this.hideSidebar = true;
+        }
+      })
+    }, 0);
+  }
 
   ngOnInit(): void {
     this.currentRoute = this.router.url.split("/")[this.router.url.split("/").length - 1]
