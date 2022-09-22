88732bc4e2eb87571b0f33701a7996c143c2faa7
Kevin <bastokevindaniel@gmail.com>
2022-09-22 12:58:17 -0500

    feat: add sidebar fix

--- src/app/layouts/client/client.component.ts
+++ src/app/layouts/client/client.component.ts
@@ -1,23 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-client',
   templateUrl: './client.component.html',
   styleUrls: ['./client.component.scss']
 })
-export class ClientComponent implements OnInit {
+export class ClientComponent implements OnInit, AfterViewInit {
+  @ViewChild(MatSidenav) sidenav!: MatSidenav;
+  toggle : boolean;
+  opened : boolean;
 
-  public toggle : boolean;
-  public opened : boolean;
+  constructor(
+    private breakpoint : BreakpointObserver
+  ) {
+    this.toggle = true;
+  }
 
-  constructor() {
-    this.toggle = false;
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.breakpoint.observe(['(max-width: 800px)']).subscribe((res) => {
+        if (res.matches) {
+          this.sidenav.mode = 'over';
+          this.sidenav.close();
+        } else {
+          this.sidenav.mode = "side"
+          this.sidenav.open();
+        }
+      })
+    }, 0);
   }
 
   ngOnInit(): void {
   }
 
-  toggleSidebar(value : boolean | any){
+  toggleSidebar(){
     this.toggle = !this.toggle;
   }
 
