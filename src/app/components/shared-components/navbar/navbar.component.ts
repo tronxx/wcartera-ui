916033bbe5d723b8c878e873fcf4c66390d20ab5
Kevin <bastokevindaniel@gmail.com>
2022-09-22 15:52:46 -0500

    feat: add implementations for the template

--- src/app/components/shared-components/navbar/navbar.component.ts
+++ src/app/components/shared-components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { AfterViewInit, Component, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 
 
 @Component({
@@ -6,11 +8,25 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit {
-
+export class NavbarComponent implements OnInit, AfterViewInit {
+  public hide : boolean = true;
   @Output() toggle : EventEmitter<boolean> = new EventEmitter<boolean>();
 
-  constructor() { }
+  constructor(
+    private breakpoint : BreakpointObserver
+  ) { }
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.breakpoint.observe(['(max-width: 800px)']).subscribe((res) => {
+        if (res.matches) {
+          this.hide = false;
+        } else {
+          this.hide = true;
+        }
+      })
+    }, 0);
+  }
 
   ngOnInit(): void {
   }
