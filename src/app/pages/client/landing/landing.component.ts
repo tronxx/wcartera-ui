5b24ee97dd79b39a26f786aaf21cb4d37f2d7576
kevin <bastokevindaniel@gmail.com>
2022-09-24 19:13:01 -0500

    feat: add landing base displaying for the landing

--- src/app/pages/client/landing/landing.component.ts
+++ src/app/pages/client/landing/landing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { InfoCard } from '@models/info-card';
 
 @Component({
   selector: 'app-landing',
@@ -6,6 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing.component.scss']
 })
 export class LandingComponent implements OnInit {
+
+  public cards : Array<InfoCard> = [
+    {
+      color : "#00ff00",
+      description: "En el último mes se han generado más de 3500 dólares de ganancia",
+      footer : "haz click para ver más",
+      icon : "shopping_cart",
+      title : "Las ventas del último més",
+      footerLink : "#"
+    },
+    {
+      color : "#6655aa",
+      description: "Los seguidores de este mes se han mantenido constantes y sonantes",
+      footer : "Los seguidores son constantes",
+      icon : "groups",
+      title : "Seguidores en redes sociales",
+    },
+    {
+      color : "#56cfaf",
+      description: "Este mes las promociones han tenido leads del 80%",
+      footer : "promos simples, grandes ganancias",
+      icon : "sell",
+      title : "Promociones"
+    }
+  ]
 
   constructor() { }
 
