e8ba70fa41e400e5973c7b455c0f707468652cbd
kevin <bastokevindaniel@gmail.com>
2022-09-26 19:11:22 -0500

    feat: page-header will now display page titles

--- src/app/layouts/client/page-header/page-header.component.ts
+++ src/app/layouts/client/page-header/page-header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
 
 @Component({
   selector: 'app-page-header',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageHeaderComponent implements OnInit {
 
-  constructor() { }
+  public route : string;
+
+  constructor(
+    private router : Router
+  ) { }
 
   ngOnInit(): void {
+    let subRoute = this.router.url.split("/")
+    environment.menu.forEach(element => {if(element.url == subRoute[2]) this.route = element.name});
+  }
+
+  trackNavigation(){
+    this.router.events.subscribe(event => {
+      if(event instanceof NavigationEnd){
+        let subRoute = this.router.url.split("/")
+        environment.menu.forEach(element => {if(element.url == subRoute[2]) this.route = element.name});
+      }
+    })
   }
 
 }
