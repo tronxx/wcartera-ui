540a6de1ffd377df716893b7e2edd76bf39df47b
kevin <bastokevindaniel@gmail.com>
2022-09-29 23:51:40 -0500

    feat: remove unused spaces in the app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { NgChartsModule } from 'ng2-charts';
 
 @NgModule({
