50e8e7d9c49da8757584079aeb4db9476d4934c3
kevin <bastokevindaniel@gmail.com>
2022-10-02 12:28:39 -0500

    feat: removed firebase cred

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,4 @@
 export const environment = {
-  firebase: {
-    projectId: 'fastbits-e4bcd',
-    appId: '1:139576086214:web:7a63100bc3e8a695c611c7',
-    storageBucket: 'fastbits-e4bcd.appspot.com',
-    apiKey: 'AIzaSyAUFiiP3hEaqZUF4LSMmFXasrm3gwf7dTs',
-    authDomain: 'fastbits-e4bcd.firebaseapp.com',
-    messagingSenderId: '139576086214',
-    measurementId: 'G-V75HB6RVRT',
-  },
   production: true,
   menu: [
     {
